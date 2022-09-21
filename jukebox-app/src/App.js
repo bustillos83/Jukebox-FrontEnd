edf3b302c0d363b1c065f874e935a980b71bdc45
angeeg <angeleenasyse@gmail.com>
2022-09-21 16:49:52 -0700

    react jukebox-app created

---
+++ jukebox-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+render(){
+  return(
+    <div>
+      <h1>This will eventually be the Jukebox app!</h1>
+    </div>
+  )
+}
+}
+
+export default App;
