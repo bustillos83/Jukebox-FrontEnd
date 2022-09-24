9cdb6dea9653e3900943704d6cc8493f5a0ac04a
Username <apkolesnyk94@gmail.com>
2022-09-24 12:01:31 -0400

    wokring on artist.js

--- jukebox-app/src/Artist.js
+++ jukebox-app/src/Artist.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 
 class Artist extends Component {
   render() {
-    console.log(this.props.music.results.artistmatches.artist.name);
+    console.log(this.props.music.topalbums.attr);
     return (
       <div>
-        <h1>Artist: {this.props.music.results.artistmatches.artist.name}</h1>
+        <h1>Artist</h1> 
+        {/* {this.props.music.topalbums.album.artist.map((data) => {
+          return (
+            <div >
+            <h2 key={data.mbid}><a href={data.url}> {data.name} </a></h2>
+           
+
+            </div>
+          )
+        })} */}
       </div>
     );
   }
